93e4c188a0d3f129552a94e39cfd02733a49e0dd
Nocelli <rafaelnocelliifsp@gmail.com>
2020-08-31 23:17:46 -0300

    :tada: - Basic project structure

---
+++ src/templates/blogPost.template.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { graphql } from 'gatsby'
+
+const blogPostTemplate = ({ data }) => {
+    const { markdownRemark } = data
+    const { frontmatter, html } = markdownRemark
+
+    return (
+        <Layout>
+            <SEO title={frontmatter.title} />
+
+            <h1>{frontmatter.title}</h1>
+            <div>Autor: {frontmatter.author}</div>
+            <div>Data: {frontmatter.date}</div>
+            <div dangerouslySetInnerHTML={{ __html: html }} />
+        </Layout>
+    )
+}
+
+export default blogPostTemplate
+
+
+export const pageQuery = graphql`
+    query($path: String!) {
+        markdownRemark(frontmatter : { path : { eq: $path }}) {
+            html
+            frontmatter {
+                path
+                title
+                author
+                date
+            }
+        }
+    }
+`
