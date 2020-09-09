8c606bf18f57f5fc1254cb0e2e3a694f9f791a3c
Nocelli <rafaelnocelliifsp@gmail.com>
2020-09-09 18:02:12 -0300

    :lipstick: - Added css to post list component

--- src/components/postList/index.js
+++ src/components/postList/index.js
@@ -1,17 +1,36 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import './styles.css'
+
 const PostList = ({ posts }) => (
-    <ul>
-        {posts && posts.map((item, index) => {
-            let post = item.node.frontmatter
-            return (
-                <li>
-                    <Link to={post.path}>{post.title}</Link>
-                </li>
-            )
-        })}
-    </ul>
+    <section className='post-list-container'>
+        <ul className='post-list'>
+            {posts && posts.map(item => {
+                let post = item.node.frontmatter
+                return (
+                    <Link
+                        key={item.node.id}
+                        to={post.path}
+                        className='post'
+                    >
+                        <figure>
+
+                        </figure>
+                        <article>
+                            <span className='post-title'>{post.title}</span>
+                            <aside className='post-text' >{post.abstract}</aside>
+                            <span className='post-date'>
+                                {
+                                    (new Date(post.date)).toLocaleDateString()
+                                }
+                            </span>
+                        </article>
+                    </Link>
+                )
+            })}
+        </ul>
+    </section>
 )
 
 export default PostList
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import PostList from "../components/postList"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Link to="/posts/">Go to posts</Link> <br />
-  </Layout>
-)
+const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(
+    graphql`
+      query {
+        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date]}) {
+          edges {
+            node {
+              id
+              frontmatter {
+                path
+                title
+                abstract
+                date
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+  return (
+    <Layout>
+      <SEO title="Home"/>
+      <PostList posts={allMarkdownRemark.edges}/>
+    </Layout>
+  )
+}
 
 export default IndexPage
