ab36c1fd007f860235aa12b4ea2214d3e9a4e8d3
Nocelli <rafaelnocelliifsp@gmail.com>
2020-09-09 18:30:25 -0300

    :lipstick: - Changes to the header component css

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,31 +1,17 @@
-import { Link } from "gatsby"
+import { Link, StaticQuery } from "gatsby"
+import BackgroundImage from 'gatsby-background-image'
 import PropTypes from "prop-types"
 import React from "react"
 
+import "./styles.css"
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <header id='header'>
+    <div>
+      <h1>
+        <Link to="/">
           {siteTitle}
-        </Link>
+        </Link> 
       </h1>
     </div>
   </header>
