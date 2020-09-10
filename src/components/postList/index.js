3d2b95994bf45bea08095b387f7035f30b0ff7e3
Ruan Montelo <ruanmontelo@gmail.com>
2020-09-09 23:41:50 -0300

    Re-styling to give the website a more spotifyer look and changing font with color scheme

--- src/components/postList/index.js
+++ src/components/postList/index.js
@@ -6,23 +6,22 @@ import './styles.css'
 const PostList = ({ posts }) => (
     <section className='post-list-container'>
         <ul className='post-list'>
-            {posts && posts.map(item => {
-                let post = item.node.frontmatter
+            {posts && posts.map(post => {
                 return (
                     <Link
-                        key={item.node.id}
-                        to={post.path}
+                        key={post.node.id}
+                        to={post.node.frontmatter.path}
                         className='post'
                     >
                         <figure>
 
                         </figure>
                         <article>
-                            <span className='post-title'>{post.title}</span>
-                            <aside className='post-text' >{post.abstract}</aside>
+                            <span className='post-title'>{post.node.frontmatter.title}</span>
+                            <aside className='post-text' >{post.node.frontmatter.abstract}</aside>
                             <span className='post-date'>
                                 {
-                                    (new Date(post.date)).toLocaleDateString()
+                                    (new Date(post.node.frontmatter.date)).toLocaleDateString()
                                 }
                             </span>
                         </article>
