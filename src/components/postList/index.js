729334f8dc451a2300d1af4c1622fd4993d52d81
Nocelli <rafaelnocelliifsp@gmail.com>
2020-09-11 13:46:02 -0300

    :lipstick: - Chenges to the overall color scheme of the page

--- src/components/postList/index.js
+++ src/components/postList/index.js
@@ -4,32 +4,34 @@ import { Link } from 'gatsby'
 import './styles.css'
 
 const PostList = ({ posts }) => (
-    <section className='post-list-container'>
-        <ul className='post-list'>
-            {posts && posts.map(post => {
-                return (
-                    <Link
-                        key={post.node.id}
-                        to={post.node.frontmatter.path}
-                        className='post'
-                    >
-                        <figure>
+    <>
+        <section className='post-list-container'>
+            <ul className='post-list'>
+                {posts && posts.map(post => {
+                    return (
+                        <Link
+                            key={post.node.id}
+                            to={post.node.frontmatter.path}
+                            className='post'
+                        >
+                            <figure>
 
-                        </figure>
-                        <article>
-                            <span className='post-title'>{post.node.frontmatter.title}</span>
-                            <aside className='post-text' >{post.node.frontmatter.abstract}</aside>
-                            <span className='post-date'>
-                                {
-                                    (new Date(post.node.frontmatter.date)).toLocaleDateString()
-                                }
-                            </span>
-                        </article>
-                    </Link>
-                )
-            })}
-        </ul>
-    </section>
+                            </figure>
+                            <article>
+                                <span className='post-title'>{post.node.frontmatter.title}</span>
+                                <aside className='post-text' >{post.node.frontmatter.abstract}</aside>
+                                <span className='post-date'>
+                                    {
+                                        (new Date(post.node.frontmatter.date)).toLocaleDateString()
+                                    }
+                                </span>
+                            </article>
+                        </Link>
+                    )
+                })}
+            </ul>
+        </section>
+    </>
 )
 
 export default PostList
